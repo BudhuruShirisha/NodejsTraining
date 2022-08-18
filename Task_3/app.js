ca4b1f8d5920216d9b44e789f14454ec138c500b
Shirisha Budhuru <sbudhuru@innominds.com>
2022-08-18 20:31:47 +0530

    updated Task 4 Files

--- Task_3/app.js
+++ Task_3/app.js
@@ -5,10 +5,9 @@ const patient = require("./routers/patient");
 const organization = require("./routers/organization");
 const common = require("./routers/common");
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+//
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, __dirname + "/temp/");
@@ -22,14 +21,12 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.use(upload.single("file"));
-
 //Configure router so all routes are prefixed with /Users
 app.use("/patient", patient);
 app.use("/organization", organization);
 app.use("/common", common);
-//app.use("/organization/contact", contact)
 
-//SET the server to listen at 3000
+//SET the server to listen at 5000
 app.listen(5000, () =>
     console.log("Node server is running on http://localhost:5000")
 );
